3023a7bf724c9d92aae8d8d0a693fead4157a734
meshiv5 <shivendra@blingg.club>
2024-10-09 19:41:00 +0530

    Empty String Will Return 0

---
+++ test/calculator.test.js
@@ -0,0 +1,13 @@
+const StringCalculator = require("../src/calculator");
+
+describe("StringCalculator", () => {
+  let calculator;
+
+  beforeEach(() => {
+    calculator = new StringCalculator();
+  });
+
+  test("should return 0 for empty string", () => {
+    expect(calculator.add("")).toBe(0);
+  });
+});
