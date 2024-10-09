6702abac0a997b29543c4aa94399450d84043671
meshiv5 <shivendra@blingg.club>
2024-10-09 19:42:06 +0530

    Single Number return same number

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -10,4 +10,8 @@ describe("StringCalculator", () => {
   test("should return 0 for empty string", () => {
     expect(calculator.add("")).toBe(0);
   });
+
+  test("should return the number for single number", () => {
+    expect(calculator.add("1")).toBe(1);
+  });
 });
